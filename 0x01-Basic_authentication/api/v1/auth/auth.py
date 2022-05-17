c6ecd7053eaa47ec3146f31b18b52a3c66e3b2ea
Yohannes251 <yohannes.z251@gmail.com>
2022-05-17 15:01:32 +0000

    Updated require_auth function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,8 +10,15 @@ class Auth:
     """Implements authentication system"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns a boolean"""
-        return False
+        """Checks if path is in excluded paths"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] == "/":
+            path = path[:-1]
+        for pat in excluded_paths:
+            if path in pat:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns a string"""
