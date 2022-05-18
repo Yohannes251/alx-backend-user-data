fd4b33bd9c4aa1a677943a62be404bca7b7f1726
Yohannes251 <yohannes.z251@gmail.com>
2022-05-18 17:50:10 +0000

    Added a new method to class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,7 +3,17 @@
     This module contains SessionAuth class
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Implements Session authentication system"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session_id for a user_id"""
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
