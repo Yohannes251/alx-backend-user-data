e5252b766d4a10da67234e49a61bf91704a3718d
Yohannes251 <yohannes.z251@gmail.com>
2022-05-26 12:28:37 +0000

    Added a new method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,9 +3,31 @@
     This module implements authorization
 """
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Encrypts passwod using bcrypt module"""
     pwd = bytes(password, 'utf-8')
     return hashpw(pwd, gensalt())
+
+
+class Auth:
+    """Implements authentication of users"""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Handles user registration"""
+
+        try:
+            check_user = self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            pwd = _hash_password(password)
+            user = self._db.add_user(email, password)
+
+            return user
