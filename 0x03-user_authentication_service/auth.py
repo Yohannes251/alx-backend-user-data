02bd78bae1fc34e2a129f3b299eea12e81f832a2
Yohannes251 <yohannes.z251@gmail.com>
2022-05-26 14:14:17 +0000

    Fixed typo

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,7 +69,7 @@ class Auth:
         if session_id is None:
             return None
         try:
-            user = self._find_user_by(session_id=session_id)
+            user = self._db.find_user_by(session_id=session_id)
             return user
         except Exception:
             return None
