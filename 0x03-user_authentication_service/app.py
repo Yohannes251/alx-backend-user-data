3688ca5e39dc6bb1fa0de153c7010cf77753da20
Yohannes251 <yohannes.z251@gmail.com>
2022-05-26 12:53:45 +0000

    Added a new route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,11 @@
 """
     This module is a basic flask app
 """
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'])
@@ -13,6 +14,18 @@ def index():
     """Index page get route"""
 
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    """Handles post request for users endpoint"""
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
