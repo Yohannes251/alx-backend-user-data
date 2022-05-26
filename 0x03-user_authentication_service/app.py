9d7340f1137abff9f2ef750a1f50965ef4da3ce9
Yohannes251 <yohannes.z251@gmail.com>
2022-05-26 12:41:35 +0000

    Created a flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    This module is a basic flask app
+"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def index():
+    """Index page get route"""
+
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
