f27055c96a65991d2e168478930faf7b8400d2fe
Yohannes251 <yohannes.z251@gmail.com>
2022-05-26 11:53:24 +0000

    Created auth module

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/python3
+"""
+    This module implements authorization
+"""
+from bcrypt import hashpw, gensalt
+
+
+def _hash_password(password: str) -> bytes:
+    """Encrypts passwod using bcrypt module"""
+    pwd = bytes(password, 'utf-8')
+    return hashpw(pwd, gensalt())
