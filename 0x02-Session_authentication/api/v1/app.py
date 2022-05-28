4b6adfc72776546b52c7f5691f275b4787b4f669
Yohannes251 <yohannes.z251@gmail.com>
2022-05-28 11:46:11 +0000

    Fixed pycodestyle error

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,17 +32,18 @@ def filter_request():
     """Filters request"""
 
     paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/']
+             '/api/v1/auth_session/login/']
 
     if auth is None or auth.require_auth(request.path, paths) is False:
         return
     request.current_user = auth.current_user(request)
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None and\
+       auth.session_cookie(request) is None:
         return None, abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
     if auth.authorization_header(request) is None:
         abort(401)
-    if auth.current_user(request) is None:
-        abort(403) 
 
 
 @app.errorhandler(404)
