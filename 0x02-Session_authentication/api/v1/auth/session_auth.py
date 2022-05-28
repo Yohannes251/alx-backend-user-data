a1293d0342aa3a4b62cd6fa5dd89b2234821f5fa
Yohannes251 <yohannes.z251@gmail.com>
2022-05-28 12:50:42 +0000

    added a method

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,8 @@
 """
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
+from typing import TypeVar
 
 
 class SessionAuth(Auth):
@@ -20,7 +22,16 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """Returns user_id based on session_id"""
-        if session_id is None or type(session_id) != str:
+        if session_id is None or type(session_id) is not str:
             return None
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
+
+    def current_user(self, request=None) -> TypeVar('User'):
+       """Returns the current user object"""
+
+       session_id = self.session_cookie(request)
+       user_id = self.user_id_for_session_id(session_id)
+       print(user_id)
+       user = User.get(user_id)
+       return user
